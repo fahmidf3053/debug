d7757bb8b8b5372de13cc6303078bf5efc42fe29
Fahmid Ahmed <fahmidahmed16@gmail.com>
2018-01-29 08:55:46 +0600

    debugging code

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+    int x=5;
+    int i;
+
+    for(i=0;i<=10;i++)
+    {
+        int z=(50/(i-x));
+    }
+
+    return 0;
+}
